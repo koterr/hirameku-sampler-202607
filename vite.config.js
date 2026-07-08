3cdc8af8eddbea9ad09d1c572acb4f603b272215
koterr <110338448+koterr@users.noreply.github.com>
2026-07-08 15:40:55 +0900

    fix: set base to repo name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/ccbt-sound-design-workshop/' : '/',
+  base: mode === 'production' ? '/hirameku-sampler-202607/' : '/',
   plugins: [mkcert()],
   build: {
     assetsInlineLimit: 0,
